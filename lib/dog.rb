f95c1fad54c74a93f3a9f3b4347824e3e373432f
Megan Dotto <megan.dotto@gmail.com>
2018-02-25 05:56:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,20 @@
 class Dog
 
-  def initialize(breed)
+  def initialize(name, breed="Mutt")
+    @name =name
     @breed = breed
+  end
+
+  def name=(name)
+    @name = name
   end
 
   def breed=(breed)
     @breed = breed
+  end
+
+  def name
+    @name
   end
 
   def breed
